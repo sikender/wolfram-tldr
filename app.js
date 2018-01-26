11c535cc0d6afefbbd37a2195e5ac7168e970198
Sikender Khan <sikendermkhan@gmail.com>
2018-01-26 17:17:04 +0530

    Add caching

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const apicache = require('apicache')
 require('dotenv').config()
 var passport = require('./auth')
 
 const app = express()
 app.use(passport.initialize())
+let cache = apicache.middleware
 
 app.use('/api/v1/query',
         passport.authenticate('bearer', { session: false }),
+        cache('10 minutes'),
         require('./routes/v1/index'))
 
 module.exports = app
