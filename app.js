c0690bf3bbf1cfc419ecdc9694bcf6cd9f731dda
Sikender Khan <sikendermkhan@gmail.com>
2018-01-26 14:32:20 +0530

    Add passport http bearer strategy

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 require('dotenv').config()
+var passport = require('./auth')
 
 const app = express()
+app.use(passport.initialize())
+
+app.get('/',
+  passport.authenticate('bearer', { session: false }),
+  function (req, res) {
+    res.send('hi')
+  })
 
 module.exports = app
